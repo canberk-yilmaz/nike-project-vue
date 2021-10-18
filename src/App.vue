bb59a60a6928183852f91c3852df5ca9ea3f81ad
canberk-yilmaz <cyilmaz089@gmail.com>
2021-10-19 00:57:44 +0300

    vue project

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,92 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Logo Here</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Careers</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <b-row class="d-flex justify-content-center">
+      <ProductCard v-for="(item, i) in productList" :key="i" class="col" />
+    </b-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductCard from "./components/ProductCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ProductCard,
+  },
+  data() {
+    return {
+      productList: [
+        {
+          title: "Title 1",
+          summary:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quam.",
+          rating: 4,
+          price: 10,
+        },
+        {
+          title: "Title 2",
+          summary: "Lorem ipsum dolor sit amet.",
+          rating: 3,
+          price: 20,
+        },
+        {
+          title: "Title 3",
+          summary: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+          price: 5,
+        },
+        {
+          title: "Title 4",
+          summary: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+          price: 5,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <b-card
+        title="Card Title"
+        img-src="../img/black/1.webp"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+};
+</script>
+
+<style scoped>
+h1 {
+  margin: 5px;
+}
+</style>

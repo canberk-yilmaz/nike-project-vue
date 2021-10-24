c28ef08fbbcc585a4a08bd464ef1b2ec3e123963
canberk-yilmaz <cyilmaz089@gmail.com>
2021-10-24 18:37:43 +0300

    Functions Complete

--- src/App.vue
+++ src/App.vue
@@ -2,51 +2,86 @@
   <div id="app">
     <nav class="d-flex box mb-5 sticky-top">
       <b-nav pills class="d-flex justify-content-center align-items-center">
-        <b-nav-item active>All Products</b-nav-item>
-        <b-nav-item class="pink">Women</b-nav-item>
-        <b-nav-item>Men</b-nav-item>
-        <!-- <b-nav-item>
-        <i class="fas fa-shopping-bag"></i>
-      </b-nav-item> -->
+        <b-nav-item
+          @click="
+            categoryMen = true;
+            categoryWomen = true;
+          "
+          >All Products</b-nav-item
+        >
+        <b-nav-item
+          @click="
+            categoryMen = false;
+            categoryWomen = true;
+          "
+          class="pink"
+          >Women</b-nav-item
+        >
+        <b-nav-item
+          @click="
+            categoryMen = true;
+            categoryWomen = false;
+          "
+          class="blue"
+          >Men</b-nav-item
+        >
       </b-nav>
       <b-dropdown id="dropdown-dropright" dropright>
         <template #button-content>
           <span class="m-2 fas fa-shopping-bag"></span>
-          <b-badge>{{ cartTotal }}</b-badge>
+          <b-badge class="d-flex align-items-center justify-content-center">{{
+            cartTotal
+          }}</b-badge>
         </template>
-        <b-dropdown-item v-for="(item, index) in cart" :key="index">
+        <div class="totalPrice" v-show="cart.length > 0">
+          Total Price : ${{ totalPrice }}
+        </div>
+        <b-dropdown-item
+          v-for="(item, index) in cart"
+          :key="index"
+          class="d-flex"
+        >
           <div v-if="item.count > 0">
-            <div>
-              {{ item.title }}
+            <div class="d-flex mb-2 border">
+              <span>
+                <img
+                  style="{height:10rem; width:10rem;}"
+                  :src="item.image"
+                  alt=""
+                />
+              </span>
+              <div>
+                <p>Size: {{ item.size }}</p>
+                <p>Qty:{{ item.count }}</p>
+                <p>Total Price: ${{ item.price * item.count }}</p>
+
+                <b-button
+                  class="mr-2"
+                  v-if="item.count > 1"
+                  @click.stop.prevent="item.count--"
+                  variant="dark"
+                  >-</b-button
+                >
+                <b-button
+                  class="mr-2"
+                  v-else-if="(item.count = 1)"
+                  @click.stop.prevent="deleteItem(index)"
+                  variant="dark"
+                  >-</b-button
+                >
+                <b-button
+                  class="mr-2"
+                  @click.stop.prevent="item.count++"
+                  variant="dark"
+                  >+</b-button
+                >
+                <b-button
+                  @click.stop.prevent="deleteItem(index)"
+                  variant="danger"
+                  >x</b-button
+                >
               </div>
-              <div>Qty:{{ item.count }} - Total Price: ${{
-              item.price * item.count }}
             </div>
-            <span>
-            <b-button
-              class="mr-2"
-              v-if="item.count > 1"
-              @click.stop.prevent="item.count--"
-              variant="dark"
-              >-</b-button
-            >
-            <b-button
-              class="mr-2"
-              v-else-if="(item.count = 1)"
-              @click.stop.prevent="deleteItem(index)"
-              variant="dark"
-              >-</b-button
-            >
-            <b-button
-              class="mr-2"
-              @click.stop.prevent="item.count++"
-              variant="dark"
-              >+</b-button
-            >
-            <b-button @click.stop.prevent="deleteItem(index)" variant="danger"
-              >x</b-button
-            >
-            </span>
           </div>
         </b-dropdown-item>
 
@@ -55,7 +90,18 @@
         </b-dropdown-item>
       </b-dropdown>
     </nav>
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div>
+      <b-alert
+        class="alert-fixed"
+        :show="dismissCountDown"
+        dismissible
+        fade
+        variant="dark"
+        @dismiss-count-down="countDownChanged"
+      >
+        Product Add
+      </b-alert>
+    </div>
     <div class="contain">
       <b-row
         class="d-flex align-items-center justify-content-center"
@@ -63,8 +109,8 @@
       >
         <ProductCard
           :product="product"
-          v-for="(product, i) in productList"
-          :key="i"
+          v-for="product in filteredByCategory()"
+          :key="product.title"
           class="col"
           @addToCart="addToCart"
         />
@@ -85,58 +131,104 @@ export default {
     return {
       productList: [
         {
-          title: "Nike React Infinity Run Flyknit 2 By You",
-          color: "Black",
+          id: "1",
+          title: "Zoom Freak 3",
+          color: ["salmon", "black", "green"],
           summary:
-            "The Nike React Infinity Run Flyknit 2 By You continues to help keep you running. A refreshed upper uses Flywire technology that combines with Flyknit for support and breathability where you need it.",
+            "Giannis is an athlete of freakish power and incredible range. His ability to play any position make him difficult to guard and nearly impossible to stop. The Zoom Freak 3 helps Giannis misdirecting Euro-step.",
           price: 140,
           images: [
-            "https://github.com/canberk-yilmaz/nike-project-vue/blob/main/src/img/black/1.webp?raw=true",
+            { img: require("./img/1.png"), color: "Darksalmon" },
+            { img: require("./img/2.png"), color: "Black" },
+            { img: require("./img/3.png"), color: "MediumTurquoise" },
           ],
-          size: [5, 6, 7, 8, 9, 10, 11, 12],
-          stock: 10,
-          category: "MEN",
+          sizes: [
+            { text: "6", value: "6" },
+            { text: "7", value: "7" },
+            { text: "8", value: "8" },
+            { text: "9", value: "9" },
+            { text: "10", value: "10" },
+            { text: "11", value: "11" },
+          ],
+          selectedSize: { text: "6", value: "6" },
+          category1: "Men",
+          category2: "Basketball",
           rating: 4,
         },
         {
-          title: "Nike Epic React Flyknit 2",
-          color: "Pearl Pink",
+          id: "2",
+          title: "Nike Air Force 1 Fontanka",
+          color: ["pink", "yellow", "greenyellow"],
           summary:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quam.",
+            "The Born out of the ideas and art of post-Soviet Union Russia, the Nike Air Force 1 Fontanka lets a new wave of comfort and style take hold. Upping the ante with its lifted midsole and layered upper that features soft leather and intricate stitching.",
           price: 150,
           images: [
-            "https://github.com/canberk-yilmaz/nike-project-vue/blob/main/src/img/pink-2/1.webp?raw=true",
+            { img: require("./img/4.png"), color: "pink" },
+            { img: require("./img/5.png"), color: "gold" },
+            { img: require("./img/6.png"), color: "#8d9458" },
           ],
-          size: [5, 6, 7, 8, 9, 10, 11, 12],
-          stock: 10,
-          category: "WOMEN",
+          sizes: [
+            { text: "6", value: "6" },
+            { text: "7", value: "7" },
+            { text: "8", value: "8" },
+            { text: "9", value: "9" },
+            { text: "10", value: "10" },
+            { text: "11", value: "11" },
+          ],
+          selectedSize: { text: "6", value: "6" },
+          category1: "Women",
+          category2: "",
           rating: 5,
         },
         {
-          title: "Nike Epic React Flyknit 3",
-          color: "Pink",
-          summary: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
-          price: 145,
-          images: [
-            "https://github.com/canberk-yilmaz/nike-project-vue/blob/main/src/img/pink-2/1.webp?raw=true",
-          ],
-          size: [5, 6, 7, 8, 9, 10, 11, 12],
-          stock: 10,
-          category: "WOMEN",
-          rating: 4,
-        },
-        {
-          title: "Nike Epic React Flyknit 4",
+          id: "3",
+          title: "Jordan Air Mae",
           color: "Blue",
-          summary: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+          summary:
+            "Future, fashion and flight intersect with the Jordan Air Mae, a shoe inspired by pioneering women. It's built on a thick-lugged outsole with plush foam and visible Nike Air cushioning.",
           price: 155,
           images: [
-            "https://github.com/canberk-yilmaz/nike-project-vue/blob/main/src/img/white/1.webp?raw=true",
+            { img: require("./img/11.png"), color: "indianred" },
+            { img: require("./img/10.png"), color: "seagreen" },
+            { img: require("./img/12.png"), color: "#827567" },
           ],
-          size: [5, 6, 7, 8, 9, 10, 11, 12],
-          stock: 10,
-          category: "MEN",
+          sizes: [
+            { text: "6", value: "6" },
+            { text: "7", value: "7" },
+            { text: "8", value: "8" },
+            { text: "9", value: "9" },
+            { text: "10", value: "10" },
+            { text: "11", value: "11" },
+          ],
+          selectedSize: { text: "6", value: "6" },
+          category1: "Women",
+          category2: "",
           rating: 5,
+        },
+        {
+          id: "4",
+          title: "Nike Air Vapormax 2021 FK",
+          color: "Pink",
+          summary:
+            "Made from at least 40% recycled materials by weight, the Nike Air VaporMax 2021 FK is airy and easy to wear with superstretchy, recycled Flyknit fabric (plus a soft collar that sculpts your ankle).",
+          price: 145,
+          images: [
+            { img: require("./img/7.png"), color: "black" },
+            { img: require("./img/8.png"), color: "skyblue" },
+            { img: require("./img/9.png"), color: "slateblue" },
+          ],
+          sizes: [
+            { text: "6", value: "6" },
+            { text: "7", value: "7" },
+            { text: "8", value: "8" },
+            { text: "9", value: "9" },
+            { text: "10", value: "10" },
+            { text: "11", value: "11" },
+          ],
+          selectedSize: { text: "6", value: "6" },
+          category1: "Men",
+          category2: "",
+          rating: 4,
         },
       ],
       catClass: {
@@ -144,27 +236,43 @@ export default {
         WOMEN: "pink",
       },
       cart: [],
-      alert: false,
-      counter: 0,
+
+      count: 0,
+      dismissSecs: 3,
+      dismissCountDown: 0,
+      showDismissibleAlert: false,
+      categoryMen: true,
+      categoryWomen: true,
     };
   },
   methods: {
     addToCart(e) {
-      let addedBefore = this.cart.filter(
-        (item) => item.title === e.title
-      ).length;
-
-      if (addedBefore) {
-        let index = this.cart.findIndex((item) => item.title === e.title);
-        console.log(index);
-        this.cart[index].count++;
+      let isFound = null;
+      isFound = this.cart.find(
+        (item) =>
+          item.id == e.id && item.color == e.color && item.size == e.size
+      );
+      if (!isFound) {
+        this.cart.push(e);
       } else {
-        this.cart.push({ ...e, count: 1 });
+        isFound.count += e.count;
       }
-      console.log(this.cart);
+      this.dismissCountDown = this.dismissSecs;
     },
     deleteItem(i) {
       this.cart.splice(i, 1);
+    },
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown;
+    },
+    filteredByCategory() {
+      if (!this.categoryWomen) {
+        return this.productList.filter((el) => el.category1 === "Men");
+      } else if (!this.categoryMen) {
+        return this.productList.filter((el) => el.category1 === "Women");
+      } else if (this.categoryMen && this.categoryWomen) {
+        return this.productList;
+      }
     },
   },
   computed: {
@@ -174,6 +282,15 @@ export default {
         count += this.cart[i].count;
       }
       return count;
+    },
+    totalPrice() {
+      if (this.cart.length === 0) {
+        return 0;
+      } else {
+        return this.cart
+          .map((item) => item.count * item.price)
+          .reduce((a, b) => a + b);
+      }
     },
   },
 };
@@ -199,6 +316,16 @@ li.nav-item {
   width: 200px;
 }
 
+.nav-item.pink:hover .child {
+  color: red;
+  border-bottom: 1px solid #111;
+}
+
+/* .nav-link:hover {
+  color: pink;
+  border-bottom: 1px solid #111;
+} */
+
 .box {
   border-bottom: 0.2rem solid #2c3e50;
   margin-bottom: 2.5rem;
@@ -212,8 +339,8 @@ li.nav-item {
 }
 
 .fa-shopping-bag.badge {
-  background-color: #f1f1f1;
-  color: #111;
+  background-color: #111;
+  color: #f6f6f6;
   font-size: 2rem;
 }
 
@@ -226,7 +353,7 @@ li.nav-item {
 }
 
 * {
-  background: #f1f1f1;
+  background: #f6f6f6;
 }
 
 .contain {
@@ -235,11 +362,15 @@ li.nav-item {
   margin-right: auto;
 }
 .btn {
-  font-size: 2rem;
+  font-size: 1rem;
+}
+
+.btn .fa-shopping-bag {
+  font-size: 3rem;
 }
 
 .btn-secondary {
-  background: #f1f1f1;
+  background: #f6f6f6;
   color: #2c3e50;
   border: none;
 }
@@ -254,5 +385,36 @@ li.nav-item {
 
 .nav-link {
   color: #2c3e50;
+}
+
+.nav-item.pink > :hover {
+  color: pink;
+}
+
+.nav-item.blue > :hover {
+  color: blue;
+}
+
+#dropdown-dropright__BV_toggle_:hover {
+  color: #2c3e50;
+}
+
+#dropdown-dropright__BV_toggle_ {
+  color: #2c3e50;
+}
+
+.totalPrice {
+  text-align: center;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+.alert-fixed {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  z-index: 9999;
+  border-radius: 0px;
+  opacity: 75%;
 }
 </style>
--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,44 +1,113 @@
 <template>
-  <div class="container">
-    <div>
-      </div><div class="col-md-6">
+  <div
+    class="container"
+    :style="{ borderColor: selectedImg.color, border: '3px solid' }"
+  >
+    <div class="col-md-6 card-item" :style="{ color: selectedImg.color }">
       <img
-        :src="product.images[0]"
+        class="card-item"
+        :src="selectedImg.img"
         :alt="product.title"
         :img-alt="product.title"
       />
-      <div class="size-select d-flex">
-        <div class="shoe-size mr-1" v-for="(n, index) in 8" :key="index">
-          {{ n + 4 }}
+      <div class="d-flex align-items-center justify-content-center mb-2">
+        <div
+          class="radio-img mr-4"
+          v-for="(item, index) in product.images"
+          :key="index"
+        >
+          <input
+            v-model="selectedImg"
+            type="radio"
+            :id="'image-' + product.id + '-' + index"
+            :name="product.id + '-image'"
+            :value="item"
+          />
+          <label
+            class="d-flex align-items-center justify-content-center"
+            :for="'image-' + product.id + '-' + index"
+          >
+            <img class="thumbnail" :src="item.img" alt="Shoes Image" />
+          </label>
         </div>
       </div>
+      <div class="d-flex font-weight-bold card-item justify-content-center">
+        Choose Your Size
+      </div>
+      <b-row>
+        <b-col col-8>
+          <b-form-group
+            v-slot="{ ariaDescribedby }"
+            class="d-flex align-items-center justify-content-center"
+          >
+            <b-form-radio
+              ref="sizeRadios"
+              button
+              button-variant="none"
+              class="radio-button"
+              v-for="(size, index) in product.sizes"
+              :key="index"
+              v-model="selectedSize"
+              :aria-describedby="ariaDescribedby"
+              name="size-radios"
+              :value="size"
+              :style="{ borderColor: selectedImg.color }"
+            >
+              <span class="size-radio">
+                {{ size.value }}
+              </span>
+            </b-form-radio>
+          </b-form-group>
+        </b-col>
+      </b-row>
+
+      <!-- <div class="mt-3">
+        Selected: <strong>{{ selectedSize.value }}</strong>
+      </div> -->
     </div>
     <div class="desc col-6">
-      <h4>{{ product.category }}'S RUNNING SHOE</h4>
-      <h3>{{ product.title }}</h3>
-      <h3>${{ product.price }}</h3>
-      <h3>{{ product.rating }}</h3>
-      <h4 v-if="product.stock > 5">In Stock</h4>
-      <h4 v-else-if="product.stock > 1">Last Few Pieces!</h4>
-      <h4 v-else-if="product.stock > 0">Last One!</h4>
-      <h4 v-else>Out of Stock</h4>
-      <p>{{ product.summary }}</p>
+      <h4 class="card-item">
+        {{ product.category1 }}'s {{ product.category2 }} Shoes
+      </h4>
+      <h3 class="card-item">{{ product.title }}</h3>
+      <h3 class="card-item">${{ calcShoePrice }}</h3>
+      <h4 class="card-item">In Stock</h4>
       <div>
-        <span
-          v-for="(item, i) in product.rating"
-          class="fa fa-star checked"
-          :key="i"
-        />
-        <span
-          v-for="(item, i) in 5 - product.rating"
-          class="fa fa-star"
-          :key="'star' - i"
-        />
-        <div class="cta">
-          <button class="cta-btn" @click="addToCart(product)">
-            <b-badge variant="light">Add To Cart</b-badge>
+        <div class="rating card-item">
+          <span
+            v-for="(item, i) in product.rating"
+            class="fa fa-star checked"
+            :key="i"
+          />
+          <span
+            v-for="(item, i) in 5 - product.rating"
+            class="fa fa-star"
+            :key="'star' - i"
+          />
+        </div>
+
+        <div class="cta card-item d-flex">
+          <select
+            class="rounded justify-content-center"
+            v-model="itemQty"
+            name="item-number"
+            id="item-number"
+          >
+            <option v-for="(i, index) in 10" :value="i" :key="index">{{
+              i
+            }}</option>
+          </select>
+          <button
+            :class="[{ disabledButton: !stockCount }, 'cta-btn']"
+            @click="addToCart(product)"
+            :disabled="!stockCount"
+          >
+            <b-badge :class="[{ disabledButton: !stockCount }]" variant="light"
+              >Add To Cart</b-badge
+            >
           </button>
         </div>
+        <h6 class="card-item">{{ product.summary }}</h6>
       </div>
     </div>
   </div>
@@ -48,9 +117,36 @@
 export default {
   name: "ProductCard",
   props: ["product"],
+  data() {
+    return {
+      selectedSize: { text: "6", value: "6" },
+      selectedImg: {
+        color: this.product.images[0].color,
+        img: this.product.images[0].img,
+      },
+      itemQty: 1,
+      stockCount: 10,
+    };
+  },
   methods: {
-    addToCart(value) {
-      this.$emit("addToCart", value);
+    addToCart() {
+      this.$emit("addToCart", {
+        id: this.product.id,
+        image: this.selectedImg.img,
+        size: this.selectedSize.value,
+        price: this.calcShoePrice,
+        color: this.selectedImg.color,
+        count: this.itemQty,
+        stockCount: this.stockCount,
+      });
+    },
+  },
+  computed: {
+    calcShoePrice() {
+      return (
+        (this.product.price * (this.selectedSize.value - 2)) / 15 +
+        75
+      ).toFixed(0);
     },
   },
 };
@@ -59,6 +155,11 @@ export default {
 <style scoped>
 * {
   background: #f6f6f6;
+}
+
+.disabledButton {
+  background-color: gray !important;
+  text-decoration-line: line-through;
 }
 
 h1 {
@@ -94,12 +195,25 @@ img {
   align-items: center;
   width: 100%;
   height: 100%;
-  border: 3px solid black;
-  color: black;
+  border: 0.15rem solid;
+  color: #2c3e50;
   font-weight: 800;
+  font-size: 1.5rem;
 }
+
+.shoe-size:hover {
+  cursor: pointer;
+}
+
 .cta-btn {
   width: 100%;
+  height: 3em;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  border: 5px solid #2c3e50;
+  border-radius: 1rem;
 }
 .desc {
   text-align: start;
@@ -118,5 +232,41 @@ img {
 }
 .container {
   background: #f6f6f6;
+  min-width: 685px;
+}
+
+.rating {
+  font-size: 1.2rem;
+}
+
+.card-item {
+  margin-bottom: 1rem;
+}
+
+.thumbnail {
+  width: 5rem;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid;
+  border-radius: 20%;
+}
+
+.sizes {
+  width: 0.5rem;
+  height: 0.5rem;
+}
+.radio-button {
+  border: 1px solid #2c3e50;
+  border-radius: 1rem;
+  display: flex;
+  align-items: center;
+  outline: none;
+}
+
+input[type="radio"] {
+  display: none;
+  outline: none;
 }
 </style>
